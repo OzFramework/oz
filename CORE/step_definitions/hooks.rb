98d34555898a6b3b1d7337f0b125899dd89855f3
Donald Bush <donald.bush@crosschx.com>
2017-05-15 17:06:07 -0400

    - Moved cucumber specific code into the step definitions - Updated logger to take only a reference to the world object ( like the other components do)

--- CORE/step_definitions/hooks.rb
+++ CORE/step_definitions/hooks.rb
@@ -2,11 +2,36 @@
 $completed_scenarios = []
 
 Before do |scenario|
-  @scenario = scenario
+
+  create_world
+
+  @logger.header "Feature: #{scenario.source.last.location}"
+  @logger.header "Scenario: #{scenario.name}".cyan
+  scenario.source.last.children.each do |step|
+    @logger.header "\t#{step.to_sexp[2..3].join(' ')}".cyan
+  end
+  @logger.header ''
+  @logger.header ''
+
 end
 
+
 After do |scenario|
+
   cleanup_world
+
   $completed_scenarios << scenario.passed?
-  log_progress($completed_scenarios)
+  @logger.header '================================ PROGRESS ================================'
+  line = ''
+  $completed_scenarios.each do |scenario_passed|
+    line += scenario_passed ? '|passed| '.green : '|failed| '.red
+    if line.length >= 144
+      @logger.header line
+      line = ''
+    end
+  end
+  @logger.header line
+  @logger.header '=========================================================================='
+  @logger.header ''
+
 end
--- EXAMPLE/step_definitions/hooks.rb
+++ EXAMPLE/step_definitions/hooks.rb
@@ -1,5 +1,5 @@
 
 Before do |scenario|
-  create_empty_world(ExampleRootPage)
+  set_root_page(ExampleRootPage)
   create_new_browser
 end
