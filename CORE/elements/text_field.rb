296bb47120eecfe7669ac1f02795bbd3191d77a4
= <=>
2017-11-14 15:59:02 -0500

    taking out fillable

--- CORE/elements/text_field.rb
+++ CORE/elements/text_field.rb
@@ -1,4 +1,4 @@
-class TextFieldElement < FillableElement
+class TextFieldElement < CoreElement
     def self.type
         :text_field
     end
@@ -14,6 +14,21 @@ class TextFieldElement < FillableElement
     def value
         assert_active
         watir_element.value
+    end
+
+    def fill(data)
+      assert_active
+      @world.logger.action "Filling [#{@name}] with [#{data}]"
+      manually_clear if @world.configuration["BROWSER"] == "internet_explorer"
+      watir_element.set(data)
+
+      begin
+        Watir::Wait.until(timeout: 1){watir_element.value == data}
+      rescue
+        raise "ERROR: Problem filling element [#{@name}] with [#{data}] value after fill was found as [#{watir_element.value}]"
+      end
+      @world.ledger.record_fill(@name, data)
+      super
     end
 
     def manually_clear
