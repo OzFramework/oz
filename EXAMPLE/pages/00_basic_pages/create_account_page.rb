14f0cca9c5ddae010150fa5d7f8e993bb107b02b
Donald Bush <greenarrowdb@gmail.com>
2018-03-04 15:39:48 -0500

    Adding static text to the create account page

--- EXAMPLE/pages/00_basic_pages/create_account_page.rb
+++ EXAMPLE/pages/00_basic_pages/create_account_page.rb
@@ -5,6 +5,15 @@ class CreateAccountPage < ExampleStorefrontRootPage
   def create_elements
 
     add_static_text(:your_personal_information, element_type: :h3, xpath: "//form[@id='account-creation_form']/div[1]/h3")
+    add_static_text(:title, element_type: :label, xpath: "//div[@class='account_creation'][1]/div[1]/label")
+    add_static_text(:first_name, element_type: :label, for: 'customer_firstname')
+    add_static_text(:last_name, element_type: :label, for: 'customer_lastname')
+    add_static_text(:email, element_type: :label, for: 'email')
+    add_static_text(:password, element_type: :label, for: 'passwd')
+    add_static_text(:password_hint, element_type: :span, xpath: "//div[@class='account_creation'][1]/div[5]/span")
+    add_static_text(:dob, element_type: :label, xpath: "//div[@class='account_creation'][1]/div[6]/label")
+    add_static_text(:newsletter, element_type: :label, for: 'newsletter')
+    add_static_text(:special_offers, element_type: :label, for: 'optin')
 
     # add_radio_button(:title, id: 'id_gender1') #TODO: add radio button support
     add_text_field(:first_name, id: 'customer_firstname')
@@ -20,6 +29,23 @@ class CreateAccountPage < ExampleStorefrontRootPage
 
     # add_checkbox(:newsletter, id: 'newsletter') #TODO: find out why these checkboxes aren't working
     # add_checkbox(:special_offers, id: 'optin')
+
+
+    add_static_text(:address_header, element_type: :h3, xpath: "//div[@class='account_creation'][2]/h3")
+    add_static_text(:address_first_name, element_type: :label, for: 'firstname')
+    add_static_text(:address_last_name, element_type: :label, for: 'lastname')
+    add_static_text(:company, element_type: :label, for: 'company')
+    add_static_text(:address_1, element_type: :label, for: 'address1')
+    add_static_text(:address_2, element_type: :label, for: 'address2')
+    add_static_text(:address_2_hint, element_type: :span, xpath: "//div[@class='account_creation'][2]/p[5]/span")
+    add_static_text(:city, element_type: :label, for: 'city')
+    add_static_text(:state, element_type: :label, for: 'id_state')
+    add_static_text(:zip, element_type: :label, for: 'postcode')
+    add_static_text(:country, element_type: :label, for: 'id_country')
+    add_static_text(:additional_information, element_type: :label, for: 'other')
+    add_static_text(:home_phone, element_type: :label, for: 'phone')
+    add_static_text(:mobile_phone, element_type: :label, for: 'phone_mobile')
+    add_static_text(:address_alias, element_type: :label, for: 'alias')
 
     add_text_field(:address_firstname, id: 'firstname')
     add_text_field(:address_lastname, id: 'lastname')
