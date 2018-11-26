f51bdabfbbacd5248c108ee6fb5c3173010d74a3
Ridge, Luke <ridgel@grangeinsurance.com>
2018-11-26 14:01:34 -0500

    allow common step defs to be referenced in third person

--- CORE/step_definitions/page_steps.rb
+++ CORE/step_definitions/page_steps.rb
@@ -1,32 +1,30 @@
 
 
-Given /^I have (.*)$/ do |target|
+Given /^(?:I have|there is a) (.*)$/ do |target|
   set_data_target(target.gsub(' ', '_').upcase)
 end
 
-Given /^I am on the (.*?) Page(?: by way of the (.*?) Page)?$/ do |target_page, intermediate_page|
+Given /^(?:I am|a user is) on the (.*?) Page(?: by way of the (.*?) Page)?$/ do |target_page, intermediate_page|
     @root_page.begin_new_session
     proceed_to(CoreUtils.find_class(intermediate_page+' Page')) if intermediate_page
     proceed_to(CoreUtils.find_class(target_page+' Page'))
     set_data_target
 end
 
-
-
-When /^I (?:proceed|go back) to the (.*?) Page(?: by way of the (.*?) Page)?$/ do |target_page, intermediate_page|
+When /^(?:I|the user) (?:proceed(?:s|)|go(?:es|) back) to the (.*?) Page(?: by way of the (.*?) Page)?$/ do |target_page, intermediate_page|
     proceed_to(CoreUtils.find_class(intermediate_page+' Page')) if intermediate_page
     proceed_to(CoreUtils.find_class(target_page+' Page'))
     set_data_target
 end
 
-When /^I fill the page with (.*)$/ do |data_name|
+When /^(?:I|they) fill the page with (.*)$/ do |data_name|
     @current_page.fill(data_name)
 end
 
-When /^I click the (.*)$/ do |element_name|
+When /^(?:I|they) click the (.*)$/ do |element_name|
   @current_page.click_on(element_name.gsub(' ','_').downcase.to_sym)
 end
 
-When /^I hover over the (.*)$/ do |element_name|
+When /^(?:I|they) hover over the (.*)$/ do |element_name|
   @current_page.hover_over(element_name.gsub(' ','_').downcase.to_sym)
 end
--- CORE/step_definitions/validation_steps.rb
+++ CORE/step_definitions/validation_steps.rb
@@ -6,14 +6,14 @@
     #Tags: @static_text
     # Checks all of the static_text objects on the page to make sure they are present and contain the proper text.
     # The data used for checking is defined in the page class and/or in the page class's .yml file.
-Then /^I can see that all the content on the page is correct$/ do
+Then /^(?:I|they) can see that all the content on the page is correct$/i do
   @current_page.validate_content
 end
 
 
     #Tags: @navigation
     # Checks that the application is on the specified page.
-Then /^I should see the (.*) Page$/ do |page|
+Then /^(?:I|they) should see the (.*) Page$/i do |page|
   page_class = CoreUtils.find_class(page+' Page')
   validate_application_is_on_page(page_class)
 end
