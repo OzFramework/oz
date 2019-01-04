ceda33540777de1db986494d249e7b0fc1dcc34c
Ridge, Luke <ridgel@grangeinsurance.com>
2019-01-04 10:29:48 -0500

    re-add the on_hover logic for now since another scenario uses it

--- EXAMPLE/pages/home_page.rb
+++ EXAMPLE/pages/home_page.rb
@@ -11,13 +11,19 @@ class HomePage < ExampleStorefrontRootPage
 
     @dresses_button = add_button(:dresses, element_type: :link, xpath: "//div[@id='block_top_menu']/ul/li[2]/a")
 
-    @casual_dresses_button = add_button(:casual_dresses, element_type: :link, xpath: "//div[@id='block_top_menu']/ul/li[2]/ul/li[1]/a")
-    @casual_dresses_button.deactivate
+    @casual_dresses_button = add_button(:casual_dresses, active: false, element_type: :link, xpath: "//div[@id='block_top_menu']/ul/li[2]/ul/li[1]/a")
 
     add_static_text(:phone_info, element_type: :span, class: 'shop-phone')
     add_static_text(:practice_header, element_type: :h1, xpath: "//div[@id='editorial_block_center']/h1")
     add_static_text(:sub_header, element_type: :h2, xpath: "//div[@id='editorial_block_center']/h2")
+    assign_element_logic
+  end
 
+  def assign_element_logic
+    @dresses_button.on_hover do
+      @casual_dresses_button.activate
+      CoreUtils.wait_until(3) { @casual_dresses_button.present? }
+    end
   end
 
   def navigate_to(menu, sub_section)
@@ -26,11 +32,6 @@ class HomePage < ExampleStorefrontRootPage
     activate :"#{sub_section}_button"
     click_on :"#{sub_section}_button"
     deactivate :"#{sub_section}_button"
-  end
-
-  def navigate_to_casual_via_hover
-    @dresses_button.hover
-    @casual_dresses_button.click
   end
 
 end
