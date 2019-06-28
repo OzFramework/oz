a727b3588719a538f8403734550972e04d39955a
SParker <sparker@igsenergy.com>
2019-06-28 14:18:58 -0400

    add extra example and logic fix

--- REVENUE/world_gadgets/api_engine.rb
+++ REVENUE/world_gadgets/api_engine.rb
@@ -15,19 +15,21 @@ module ApiEngine
       fail 'type should be a symbol' unless type.is_a?(Symbol)
 
       request_info(endpoint, args)
-      @response = HTTPI.request(type, @request, adapter = @adapter)
+      @@response = HTTPI.request(type, @request, adapter = @adapter)
     end
 
     def request_info(endpoint, args)
-      args = args[0]
-      authenticate(args[:auth_type], args) unless args[:auth_type].nil?
-      @adapter = args[:adapter] unless args[:adapter].nil?
+      unless args.empty?
+        args = args[0]
+        authenticate(args[:auth_type], args) unless args[:auth_type].nil?
+        @adapter = args[:adapter] unless args[:adapter].nil?
+        @request.body = args[:body] unless args[:body].nil?
+        @request.headers = args[:headers] unless args[:headers].nil?
+        @request.query = args[:query] unless args[:query].nil?
+      end
       #This is used to since we don't have any security :(
       @request.auth.ssl.verify_mode = :none
       @request.url = endpoint
-      @request.body = args[:body] unless args[:body].nil?
-      @request.headers = args[:headers] unless args[:headers].nil?
-      @request.query = args[:query] unless args[:query].nil?
     end
 
     def authenticate(type, *args)
@@ -50,35 +52,16 @@ module ApiEngine
     end
 
     ################RESPONSE SECTION####################
-    def raw_body
-      fail "Must have a proper request to return the #{__method__}" unless @response.nil?
-      @response.raw_body
-    end
-
-    def body
-      fail "Must have a proper request to return the #{__method__}" unless @response.nil?
-      @response.body.to_json
-    end
-
     def hash_body
       fail "Must have a proper request to return the #{__method__}" unless @response.nil?
-      JSON.parse(@response.body)
+      JSON.parse(@@response.body)
     end
 
-    def code
-      fail "Must have a proper request to return the #{__method__}" unless @response.nil?
-      @response.code
+    ############VALIDATION SECTION#############
+    def success_code?
+      @@response.code.between?(200, 299)
     end
 
-    def headers
-      fail "Must have a proper request to return the #{__method__}" unless @response.nil?
-      @response.headers
-    end
-
-    def error?
-      fail "Must have a proper request to return the #{__method__}" unless @response.nil?
-      @response.error?
-    end
   end
 
   class Soap
@@ -87,5 +70,7 @@ module ApiEngine
 end
 
 call = ApiEngine::Rest.new
-response = call.request('https://revenueenterpriseservices.int.igsenergy.net/BillingAccount/GetInformation', :post, body: '{"InvoiceGroupID" : 4899259}')
-response.code
+# response = call.request('https://revenueenterpriseservices.int.igsenergy.net/BillingAccount/GetInformation', :post, body: '{"InvoiceGroupID" : 4899259}') #post example
+call.request('https://revenueenterpriseservices.int.igsenergy.net/Invoices/ExcelSummarySheetData/22644713', :get) #get example
+fail 'Response length must be greater than 0' unless call.hash_body['FileData'].length > 0 #get example validation
+fail "Call failed with #{call.code} response code" unless call.success_code?
