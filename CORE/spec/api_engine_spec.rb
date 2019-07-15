82d80bc32073f28652e2a7291cd437ea96136c92
Sparker <shadd.parker@gmail.com>
2019-07-15 14:25:39 -0400

    update spec and api_engine.rb file

--- CORE/spec/api_engine_spec.rb
+++ CORE/spec/api_engine_spec.rb
@@ -1,11 +1,48 @@
 require 'rspec'
-require '/world_gadgets/api_engine'
+require_relative '../world_gadgets/api_engine'
 
 describe ApiEngine do
-  context 'it is implemented' do
-    it 'should have unit tests defined' do
-      pending 'Not implemented'
-    end
-  end
-end
+  context 'Rest Calls' do
 
+    before(:all) do
+      @call = ApiEngine::Rest.new
+    end
+
+    it '.request - get' do
+      @call.request('https://postman-echo.com/get?foo1=bar1&foo2=bar2', :get)
+      expect(@call.code).to eq(200)
+      expect(@call.body.length).to be > 0
+    end
+
+    it '.request - post' do
+      @call.request('https://postman-echo.com/post', :post)
+      expect(@call.code).to eq(200)
+      expect(@call.body.class).to be(String)
+    end
+
+    it 'returns a proper error code' do
+      @call.request('https://postman-echo.com/get?foo1=bar1&foo2=bar2', :post, :body => {CutestId: 63})
+      expect(@call.error?).to be true
+    end
+
+    it 'returns a proper response code' do
+      @call.request('https://postman-echo.com/get?foo1=bar1&foo2=bar2', :get)
+      expect(@call.code.class).to be(Integer)
+    end
+
+    it 'body should be a hash' do
+      @call.request('https://postman-echo.com/get?foo1=bar1&foo2=bar2', :get)
+      expect(@call.code).to eq(200)
+      expect(@call.body_hash.class).to be(Hash)
+    end
+
+    it 'body should be a String' do
+      @call.request('https://postman-echo.com/get?foo1=bar1&foo2=bar2', :get)
+      expect(@call.code).to eq(200)
+      expect(@call.body.class).to be(String)
+    end
+
+  end
+
+
+end
--- CORE/world_gadgets/api_engine.rb
+++ CORE/world_gadgets/api_engine.rb
@@ -1,9 +1,10 @@
-require_relative '../../utils/oz_loader'
-OzLoader.check_gems(%w[httpi], 'APIEngine')
-require 'httpi'
+# require_relative '../../utils/oz_loader'
+# OzLoader.check_gems(%w[httpi], 'APIEngine')
 require 'json'
+require 'httpi'
 
 module ApiEngine
+
   class Rest
 
     def self.world_name
@@ -11,27 +12,52 @@ module ApiEngine
     end
 
     def initialize(world = nil)
-      ensure_installed('HTTPI')
       @world = world
       @request = HTTPI::Request.new
+      @request.headers["content-type"] = "application/json"
     end
 
     ###############REQUEST SECTION#############
-    def request(endpoint = nil, type, *args)
-      fail 'must provide an endpoint' if endpoint.nil?
+    def request(endpoint = nil, type, **args)
+      fail 'must provide an endpoint' unless !endpoint.nil?
       #type can be put, post, get, delete, or head
       fail 'type should be a symbol' unless type.is_a?(Symbol)
-
       payload_info(endpoint, args)
       @response = HTTPI.request(type, @request, adapter = @adapter)
     end
 
-    def payload_info(endpoint, args)
+    ################RESPONSE SECTION###################
+    def body_hash
+      fail "Must have a proper request to return the body" unless !@response.nil?
+      JSON.parse(@response.body)
+    end
+
+    def response
+      #return full response exactly as it came back from the resource
+      @response
+    end
+
+    def code
+      @response.code.to_i
+    end
+
+    def body
+      @response.body
+    end
+
+    ############VALIDATION SECTION#############
+    def error?
+      @response.error?
+    end
+
+    private
+
+    def payload_info(endpoint, *args)
       unless args.empty?
         args = args[0]
         @env = args[:env]
         @adapter = args[:adapter] unless args[:adapter].nil?
-        @request.body = args[:body] unless args[:body].nil?
+        @request.body = args[:body].to_json unless args[:body].nil?
         @request.headers = args[:headers] unless args[:headers].nil?
         @request.query = args[:query] unless args[:query].nil?
         authenticate(args[:auth_type], args) unless args[:auth_type].nil?
@@ -41,9 +67,9 @@ module ApiEngine
     end
 
     def authenticate(type, args)
-      user = args[:username] if args[:username].nil?
-      pass = args[:password] if args[:password].nil?
-      domain = args[:domain] if args[:domain].nil?
+      user = args[:username] unless !args[:username].nil?
+      pass = args[:password] unless !args[:password].nil?
+      domain = args[:domain] unless !args[:domain].nil?
 
       case type
       when 'basic'
@@ -56,8 +82,7 @@ module ApiEngine
         @request.auth.ssl.ca_cert_file = __dir__ + "/#{cert_file}"
       when 'digest'
         request.auth.digest(user, pass)
-      else
-        #use no verification if nothing is given
+      when 'none'
         @request.auth.ssl.verify_mode = :none
       end
     end
@@ -66,37 +91,19 @@ module ApiEngine
       Dir.glob('*/*').grep(/#{@env}/)[0]
     end
 
-    ################RESPONSE SECTION###################
-    def body_hash
-      fail "Must have a proper request to return the body" if @response.nil?
-      JSON.parse(@response.body)
-    end
-
-    def response
-      #return full response exactly as it came back from the resource
-      @response
-    end
-
-    def code
-      @response.code
-    end
-
-    def body
-      @response.body
-    end
-
-    ############VALIDATION SECTION#############
-    def error?
-      @response.error?
-    end
-
   end
-
 end
 
 #test calls and validation checks
+# Initiate APIEngine
 # call = ApiEngine::Rest.new
+
+# Test calls for post and get
 # call.request('https://revenueenterpriseservices.int.igsenergy.net/BillingAccount/GetInformation', :post, body: '{"InvoiceGroupID" : 4899259}') #post example
+# call.request('https://productmanagement.INT.igsenergy.net/rating/search', :post, auth_type: 'ssl', env: 'int', body: {CutestId: 63, RateCode: 'QA-PP-FIXED-CF-TEST3'})
 # call.request('https://revenueenterpriseservices.int.igsenergy.net/Invoices/ExcelSummarySheetData/22644713', :get, :auth_type => 'ssl', :env => 'int') #get example
+
+# Testing fail calls
 # fail 'Response length must be greater than 0' unless call.body_hash['FileData'].length > 0 #get example validation
-# fail "Call failed with #{call.response.code} response code" unless !call.error?
+# fail "Call failed with #{call.response.code} response code" if call.error?
+# p call.body_hash
