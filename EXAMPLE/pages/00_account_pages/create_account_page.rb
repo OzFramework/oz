0125d069b856189d80de220909fc7915eeacf436
Luke Ridge <ridgel@grangeinsurance.com>
2019-07-17 08:32:02 -0400

    fix a page duplication

--- EXAMPLE/pages/00_account_pages/create_account_page.rb
+++ EXAMPLE/pages/00_account_pages/create_account_page.rb
@@ -1,12 +1,20 @@
-class CreateAccountPage < ExampleStorefrontRootPage
+class CreateAccountWithParentsPage < ExampleStorefrontRootPage
 
   add_id_element(:h1, /CREATE AN ACCOUNT/, class: 'page-heading')
   add_route(:MyAccountPage, :register_account, :fill)
 
   def create_elements
 
-    add_static_text(:your_personal_information, element_type: :h3, xpath: "//form[@id='account-creation_form']/div[1]/h3")
-    add_static_text(:title, element_type: :label, xpath: "//div[@class='account_creation'][1]/div[1]/label")
+    account_creation_form = add_element(:account_creation_form, element_type: :form, id: 'account-creation_form')
+    personal_information_section = add_element(:personal_information_section, element_type: :div, parent: account_creation_form, class: 'account_creation', index: 0)
+    address_section = add_element(:address_section, element_type: :div, parent: account_creation_form, class: 'account_creation', index: 1)
+    date_of_birth_section = add_element(:date_of_birth_section, parent: personal_information_section, class: 'form-group', index: 4)
+
+    # Due to the fact that there's only one h3 under the personal information section, we don't have to be very specific now.  Only reason we need the class
+    # is because oz requires a selector.
+    add_static_text(:your_personal_information, element_type: :h3, parent: personal_information_section, class: 'page-subheading')
+    # This label is the first one in the section
+    add_static_text(:title, element_type: :label, parent: personal_information_section, index: 0)
     add_static_text(:mr, element_type: :label, for: "id_gender1")
     add_static_text(:mrs, element_type: :label, for: "id_gender2")
 
@@ -14,8 +22,9 @@ class CreateAccountPage < ExampleStorefrontRootPage
     add_static_text(:last_name, element_type: :label, for: 'customer_lastname')
     add_static_text(:email, element_type: :label, for: 'email')
     add_static_text(:password, element_type: :label, for: 'passwd')
-    add_static_text(:password_hint, element_type: :span, xpath: "//div[@class='account_creation'][1]/div[5]/span")
-    add_static_text(:dob, element_type: :label, xpath: "//div[@class='account_creation'][1]/div[6]/label")
+    # There are two form_info class'd spans on the page, however there is only one that is a child of the personal information section.
+    add_static_text(:password_hint, element_type: :span, parent: personal_information_section, class: 'form_info')
+    add_static_text(:dob, element_type: :label, parent: date_of_birth_section, index: 0)
     add_static_text(:newsletter, element_type: :label, for: 'newsletter')
     add_static_text(:special_offers, element_type: :label, for: 'optin')
 
@@ -32,14 +41,15 @@ class CreateAccountPage < ExampleStorefrontRootPage
     add_checkbox(:newsletter, id: 'newsletter')
     add_checkbox(:special_offers, id: 'optin')
     
-    add_static_text(:address_header, element_type: :h3, xpath: "//div[@class='account_creation'][2]/h3")
+    add_static_text(:address_header, element_type: :h3, parent: address_section, class: 'page-subheading')
     add_static_text(:address_first_name, element_type: :label, for: 'firstname')
     add_static_text(:address_last_name, element_type: :label, for: 'lastname')
     add_static_text(:company, element_type: :label, for: 'company')
     add_static_text(:address_1, element_type: :label, for: 'address1')
-    add_static_text(:address_1_hint, element_type: :span, xpath: "//div[@class='account_creation'][2]/p[4]/span")
+    # We can actually constrain x-paths using parents too.
+    add_static_text(:address_1_hint, element_type: :span, parent: address_section, xpath: "./p[4]/span")
     add_static_text(:address_2, element_type: :label, for: 'address2')
-    add_static_text(:address_2_hint, element_type: :span, xpath: "//div[@class='account_creation'][2]/p[5]/span")
+    add_static_text(:address_2_hint, element_type: :span, parent: address_section, xpath: "./p[5]/span")
     add_static_text(:city, element_type: :label, for: 'city')
     add_static_text(:state, element_type: :label, for: 'id_state')
     add_static_text(:zip, element_type: :label, for: 'postcode')
@@ -48,7 +58,7 @@ class CreateAccountPage < ExampleStorefrontRootPage
     add_static_text(:home_phone, element_type: :label, for: 'phone')
     add_static_text(:mobile_phone, element_type: :label, for: 'phone_mobile')
     add_static_text(:address_alias, element_type: :label, for: 'alias')
-    add_static_text(:required_field, element_type: :span, xpath: '//form[@id="account-creation_form"]/div[4]/p/span')
+    add_static_text(:required_field, element_type: :span, parent: account_creation_form, xpath: './div[4]/p/span')
 
     add_text_field(:address_firstname, id: 'firstname')
     add_text_field(:address_lastname, id: 'lastname')
