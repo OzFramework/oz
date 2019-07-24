89b621772d6e8cc3fb525bba0bea219a00cf6c10
Luke Ridge <ridgel@grangeinsurance.com>
2019-07-24 11:25:47 -0400

    respace to 2 spaces

--- lib/oz/world_extensions/world_navigation.rb
+++ lib/oz/world_extensions/world_navigation.rb
@@ -5,21 +5,21 @@ module Oz
 
     def proceed_to(target_page)
       return if @current_page.class == target_page
-        routes = @router.get_routes_between(@current_page.class, target_page)
-        raise "No path found from [#{@current_page.class}] to [#{target_page}]" unless routes
-        routes.each do |route|
-          @current_page.navigate_via_route(route)
-        end
+      routes = @router.get_routes_between(@current_page.class, target_page)
+      raise "No path found from [#{@current_page.class}] to [#{target_page}]" unless routes
+      routes.each do |route|
+        @current_page.navigate_via_route(route)
+      end
     end
 
     def assert_and_set_page(page_name)
       @router.wait_for_page_to_load(page_name)
-        @router.assert_application_is_on_page(page_name)
-        unless @current_page.class == page_name
-          @current_page = page_name.new(self)
-            @ledger.save_page_visit(page_name)
-            @current_page.on_page_load
-        end
+      @router.assert_application_is_on_page(page_name)
+      unless @current_page.class == page_name
+        @current_page = page_name.new(self)
+        @ledger.save_page_visit(page_name)
+        @current_page.on_page_load
+      end
     end
 
     def validate_application_is_on_page(page_class)
