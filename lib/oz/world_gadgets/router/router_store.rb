915454c211c4d420bd38b30fb5dbae55887b76d6
Luke Ridge <ridgel@grangeinsurance.com>
2019-07-26 08:15:49 -0400

    create a page registry and refactor router-store to use eiganclass instance variables instead of class variables per ruby styleguide

--- lib/oz/world_gadgets/router/router_store.rb
+++ lib/oz/world_gadgets/router/router_store.rb
@@ -1,52 +1,58 @@
 module Oz
   class RouterStore
 
-    @@stored_blueprints = {}
-    @@stored_routes = []
-    @@stored_elements = {}
+    class << self
+      attr_reader :registry
+    end
 
-    def self.store_page_blueprint(pagename, parents)
-      @@stored_blueprints[pagename] = PageBlueprint.new(pagename, parents)
+    @registry = []
+
+    @stored_blueprints = {}
+    @stored_routes = []
+    @stored_elements = {}
+
+    def self.store_page_blueprint(page_name, parents)
+      @stored_blueprints[page_name] = PageBlueprint.new(page_name, parents)
     end
 
     def self.store_route(source_page, target_page, action, prerequisites)
-      @@stored_routes.push({source_page: source_page,
-                            target_page: target_page,
-                            action: action,
-                            prerequisites: prerequisites})
+      @stored_routes.push(source_page: source_page,
+                          target_page: target_page,
+                          action: action,
+                          prerequisites: prerequisites)
     end
 
     def self.store_element(page, options_hash, action, type)
-      @@stored_elements[page] ||= []
-      @@stored_elements[page].push({options_hash: options_hash, action: action, type: type})
+      @stored_elements[page] ||= []
+      @stored_elements[page].push(options_hash: options_hash, action: action, type: type)
     end
 
     def self.store_id_element(page, element_type, id_hash, action)
       options_hash = { element_type: element_type }.merge(id_hash)
-      self.store_element(page, options_hash, action, :id)
+      store_element(page, options_hash, action, :id)
     end
 
     def self.store_wait_element(page, element_type, id_hash, action)
       options_hash = { element_type: element_type }.merge(id_hash)
-      self.store_element(page, options_hash, action, :wait)
+      store_element(page, options_hash, action, :wait)
     end
 
     def self.generate_graph(world)
 
-      #Create copy of stored blueprints
+      # Create copy of stored blueprints
       page_blueprints = {}
-      @@stored_blueprints.each_pair do |pagename, blueprint|
+      @stored_blueprints.each_pair do |pagename, blueprint|
         page_blueprints[pagename] = blueprint.empty_copy
       end
 
-      #Copy Routes into blueprints
-      @@stored_routes.each do |route|
-        target_page = CoreUtils.find_class( route[:target_page] )
-        page_blueprints[ route[:source_page] ].add_route( target_page, route[:action], route[:prerequisites])
+      # Copy Routes into blueprints
+      @stored_routes.each do |route|
+        target_page = registry.find { |it| it.to_s =~ /#{route[:target_page]}/i }
+        page_blueprints[route[:source_page]].add_route(target_page, route[:action], route[:prerequisites])
       end
 
-      #Copy Elements into blueprints
-      @@stored_elements.each_pair do |page, data|
+      # Copy Elements into blueprints
+      @stored_elements.each_pair do |page, data|
         data.each do |item|
           element = CoreElement.new(:ident, world, item[:options_hash])
           page_blueprints[page].add_wait_element(element, item[:action]) if item[:type] == :wait
@@ -54,21 +60,21 @@ module Oz
         end
       end
 
-      #Inherit all details from parents
+      # Inherit all details from parents
       parents = []
       page_blueprints.values.each do |blueprint|
         blueprint.parents.each do |parent|
           blueprint.inherit_details(page_blueprints[parent])
-          parents.push( parent )
+          parents.push(parent)
         end
       end
 
-      #Remove all parents from graph
+      # Remove all parents from graph
       parents.uniq.each do |parent|
         page_blueprints.delete(parent)
       end
 
-      return page_blueprints
+      page_blueprints
     end
 
   end
